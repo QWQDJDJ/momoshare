66f0773e9fb4490155c5487d4ddd7ad83a61fafa
QWQDJDJ <166776598+QWQDJDJ@users.noreply.github.com>
2024-04-12 11:37:10 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=32454734&pid=fffd38c439c82a10bbadccc9b2e328bf&tid=239a519ceab1eed4a89cd7e3a9e1bd98",
+    "https://www.maimemo.com/share/page?uid=30006451&pid=bc3cdd0661f4d7117beffd3b16200b77&tid=a4ea90462e6dbd1b7470b08a58a88ece",
 
 
   ]
